ebb93b12eb2f42d53c1329c836039e2d9a48dbfa
cdj68765 <cdj68765@163.com>
2017-05-15 01:24:56 +0800

    Start

---
+++ NyaaAnalyse/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace NyaaAnalyse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var Web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument HtmlDoc = Web.Load("https://sukebei.nyaa.si/");
+            HtmlNodeCollection nodes = HtmlDoc.DocumentNode.SelectNodes("//li/h3/a[@href]");
+            foreach (var item in nodes)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
