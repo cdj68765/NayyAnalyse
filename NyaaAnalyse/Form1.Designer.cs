86198be6c350418b2a9fa7aae040d9c83c408f8a
cdj68765 <cdj68765@163.com>
2017-05-19 01:34:08 +0800

    05-19

--- NyaaAnalyse/Form1.Designer.cs
+++ NyaaAnalyse/Form1.Designer.cs
@@ -29,11 +29,115 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.darkMenuStrip1 = new DarkUI.Controls.DarkMenuStrip();
+            this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出 = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel2 = new DarkUI.Controls.DarkLabel();
+            this.darkLabel3 = new DarkUI.Controls.DarkLabel();
+            this.darkMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Anime.png");
+            this.imageList1.Images.SetKeyName(1, "AV.png");
+            this.imageList1.Images.SetKeyName(2, "Doujinshi.png");
+            this.imageList1.Images.SetKeyName(3, "Game.png");
+            this.imageList1.Images.SetKeyName(4, "Manga.png");
+            // 
+            // darkMenuStrip1
+            // 
+            this.darkMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(63)))), ((int)(((byte)(65)))));
+            this.darkMenuStrip1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.菜单ToolStripMenuItem});
+            this.darkMenuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.darkMenuStrip1.Name = "darkMenuStrip1";
+            this.darkMenuStrip1.Padding = new System.Windows.Forms.Padding(3, 2, 0, 2);
+            this.darkMenuStrip1.Size = new System.Drawing.Size(698, 25);
+            this.darkMenuStrip1.TabIndex = 0;
+            this.darkMenuStrip1.Text = "darkMenuStrip1";
+            // 
+            // 菜单ToolStripMenuItem
+            // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.退出});
+            this.菜单ToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
+            this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.菜单ToolStripMenuItem.Text = "菜单";
+            // 
+            // 退出
+            // 
+            this.退出.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.退出.Name = "退出";
+            this.退出.Size = new System.Drawing.Size(100, 22);
+            this.退出.Text = "退出";
+            this.退出.Click += new System.EventHandler(this.退出_Click);
+            // 
+            // darkLabel1
+            // 
+            this.darkLabel1.AutoSize = true;
+            this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel1.Location = new System.Drawing.Point(184, 77);
+            this.darkLabel1.Name = "darkLabel1";
+            this.darkLabel1.Size = new System.Drawing.Size(65, 12);
+            this.darkLabel1.TabIndex = 1;
+            this.darkLabel1.Text = "darkLabel1";
+            // 
+            // darkLabel2
+            // 
+            this.darkLabel2.AutoSize = true;
+            this.darkLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel2.Location = new System.Drawing.Point(186, 93);
+            this.darkLabel2.Name = "darkLabel2";
+            this.darkLabel2.Size = new System.Drawing.Size(65, 12);
+            this.darkLabel2.TabIndex = 2;
+            this.darkLabel2.Text = "darkLabel2";
+            // 
+            // darkLabel3
+            // 
+            this.darkLabel3.AutoSize = true;
+            this.darkLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.darkLabel3.Location = new System.Drawing.Point(188, 109);
+            this.darkLabel3.Name = "darkLabel3";
+            this.darkLabel3.Size = new System.Drawing.Size(65, 12);
+            this.darkLabel3.TabIndex = 3;
+            this.darkLabel3.Text = "darkLabel3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(698, 366);
+            this.Controls.Add(this.darkLabel3);
+            this.Controls.Add(this.darkLabel2);
+            this.Controls.Add(this.darkLabel1);
+            this.Controls.Add(this.darkMenuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.darkMenuStrip1;
+            this.Name = "Form1";
+            this.Text = "Browse :: Sukebei";
+            this.darkMenuStrip1.ResumeLayout(false);
+            this.darkMenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.ImageList imageList1;
+        private DarkUI.Controls.DarkMenuStrip darkMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出;
+        private DarkUI.Controls.DarkLabel darkLabel1;
+        private DarkUI.Controls.DarkLabel darkLabel2;
+        private DarkUI.Controls.DarkLabel darkLabel3;
     }
 }
 
--- NyaaAnalyse/Form1.cs
+++ NyaaAnalyse/Form1.cs
@@ -14,107 +14,146 @@ using System.IO;
 using System.Net;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Data.Common;
 
 namespace NyaaAnalyse
 {
-    public partial class Form1 : Form
+    public partial class Form1 : DarkUI.Forms.DarkForm
     {
         private HtmlAgilityPack.HtmlDocument HtmlDoc = new HtmlAgilityPack.HtmlDocument();
 
         public Form1()
         {
             InitializeComponent();
-             SqlConnection c = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\m10\Documents\Nyaa.mdf;Integrated Security=True;Connect Timeout=30");
+            SQLiteConnection.CreateFile("Nyaa");
+            SQLiteConnection connection = new SQLiteConnection(@"data source=C:\Users\cdj68\Source\Repos\NayyAnalyse\NyaaAnalyse\Nyaa");
+            connection.Open();
+            SQLiteCommand command = new SQLiteCommand(connection);
+            /* using (SQLiteCommand command = new SQLiteCommand(connection))
+             {
+                 command.CommandText = "CREATE TABLE torrent(CLass title ImageSrc Address Name Torrent Size Time Up Leeches Complete)";
+                 command.ExecuteNonQuery();
+             }*/
+            /* command.CommandText = "DROP  TABLE  torrent";//删除表
+             command.ExecuteScalar();*/
+           command.CommandText = "create table torrent(CLass char,title char ,Address char,Name nvarchar(50),Torrent char,Magnet nvarchar(50),Size char,Time char,Up char,Leeches char,Complete char)";
+            command.ExecuteScalar();
+         //  command.CommandText = "CREATE UNIQUE INDEX idx_something ON  torrent(CLass ,title  ,Address ,Name ,Torrent ,Magnet ,Size ,Time ,Up ,Leeches ,Complete )";
+          //  command.ExecuteScalar();
+            //SqlConnection c = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\cdj68\Desktop\PEPlugin-Git\bin\Debug\Nyaa.mdf;Integrated Security=True;Connect Timeout=30");
             //SqlConnection c = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\m10\Source\Repos\NayyAnalyse\NyaaAnalyse\Nyaa.mdf;Integrated Security=True;Connect Timeout=30");
             HtmlDoc.LoadHtml(Resource1.Html);
-            c.Open();
-            SqlCommand Cmd = new SqlCommand();
-            Cmd.Connection = c;
-            Cmd.CommandType = CommandType.Text;
-         /*   Cmd.CommandText = "delete from torrent";
+            /* c.Open();
+             SqlCommand Cmd = new SqlCommand();
+             Cmd.Connection = c;
+             Cmd.CommandType = CommandType.Text;*/
+            /*   Cmd.CommandText = "delete from torrent";
+               Cmd.ExecuteScalar();*/
+            /*Cmd.CommandText = "create table torrent(CLass varchar(50),title varchar(50),ImageSrc varchar(50),Address varchar(varchar),Name nvarchar(50),Torrent varchar(50),Magnet varchar(50),Size varchar(50),Time varchar(50),Up varchar(50),Leeches varchar(50),Complete char(50))";
             Cmd.ExecuteScalar();*/
-             /*Cmd.CommandText = "create table torrent(CLass varchar(100),title varchar(100),ImageSrc char(100),Address varchar(100),Name varchar(100),Torrent char(100),Magnet varchar(1024),Size varchar(100),Time char(100),Up varchar(100),Leeches varchar(100),Complete char(100))";
-             Cmd.ExecuteScalar();*/
             HtmlNodeCollection hrefs2 = HtmlDoc.DocumentNode.SelectNodes(@" / html[1] / body[1] / div[1] / div[2] / table[1] / tbody[1] / tr");
-            foreach (var item in hrefs2)
+            Control.CheckForIllegalCrossThreadCalls = false;
+            new Task(() =>
             {
-                var temp = HtmlNode.CreateNode(item.OuterHtml);
-                var Class = item.Attributes["class"].Value;
-                var title = temp.SelectSingleNode(@"//a[1]").Attributes["title"].Value;
-                var Imgsrc = temp.SelectSingleNode("//img").Attributes["src"].Value;
-                var Address = HtmlNode.CreateNode(temp.SelectSingleNode(@"//td[2]").InnerHtml).SelectSingleNode("//a[1]").Attributes["href"].Value;
-                var name = temp.SelectSingleNode(@"//td[2]").InnerText;
-                var Torrent = HtmlNode.CreateNode(temp.SelectSingleNode(@"//td[3]").InnerHtml).SelectSingleNode("//a[1]").Attributes["href"].Value;
-                var Magnet = "";
-                if (Torrent.StartsWith("magnet"))
+                Stopwatch stopWatch = new Stopwatch();
+                DbTransaction trans = connection.BeginTransaction();
+                stopWatch.Start();
+                var Count = 0;
+                for (int i = 0; i < 100; i++)
                 {
-                    Magnet = Torrent;
-                    Torrent = "";
+                   
+                    darkLabel1.Text = i.ToString() ;
+                    darkLabel2.Text = stopWatch.Elapsed.Minutes.ToString();
+                    darkLabel3.Text =(stopWatch.Elapsed.Milliseconds - Count).ToString();
+                     Count = stopWatch.Elapsed.Milliseconds; 
+                    foreach (var item in hrefs2)
+                    {
+                        var temp = HtmlNode.CreateNode(item.OuterHtml);
+                        var Class = item.Attributes["class"].Value;
+                        var title = temp.SelectSingleNode(@"//a[1]").Attributes["title"].Value;
+                        var Imgsrc = temp.SelectSingleNode("//img").Attributes["src"].Value;
+                        var Address = HtmlNode.CreateNode(temp.SelectSingleNode(@"//td[2]").InnerHtml).SelectSingleNode("//a[1]").Attributes["href"].Value;
+                        var name = temp.SelectSingleNode(@"//td[2]").InnerText;
+                        var Torrent = HtmlNode.CreateNode(temp.SelectSingleNode(@"//td[3]").InnerHtml).SelectSingleNode("//a[1]").Attributes["href"].Value;
+                        var Magnet = "";
+                        if (Torrent.StartsWith("magnet"))
+                        {
+                            Magnet = Torrent;
+                            Torrent = "";
+                        }
+                        else
+                        {
+                            Magnet = HtmlNode.CreateNode(temp.SelectSingleNode(@"//td[3]").InnerHtml).SelectSingleNode("//a[2]").Attributes["href"].Value;
+                        }
+                        var Size = temp.SelectSingleNode(@"//td[4]").InnerHtml;
+                        var Time = temp.SelectSingleNode(@"//td[5]").InnerHtml;
+                        var Up = temp.SelectSingleNode(@"//td[6]").InnerHtml;
+                        var Leeches = temp.SelectSingleNode(@"//td[7]").InnerHtml;
+                        var Complete = temp.SelectSingleNode(@"//td[8]").InnerHtml;
+                        var TB = new StringBuilder();
+                        TB.Append(@"insert or ignore into torrent(CLass,title,Address,Name,Torrent,Magnet,Size,Time,Up,Leeches,Complete) values(");
+                        TB.Append("'");
+                        TB.Append(new Random(BitConverter.ToInt32(Guid.NewGuid().ToByteArray(), 0)).Next(10000));
+                        TB.Append("',");
+
+                        TB.Append("'");
+                        TB.Append(title);
+                        TB.Append("',");
+
+                        /* TB.Append("'");
+                         TB.Append(Imgsrc);
+                         TB.Append("',");*/
+
+                        TB.Append("'");
+                        TB.Append(Address);
+                        TB.Append("',");
+
+                        TB.Append("\'" + name + "\',");
+
+                        TB.Append("'");
+                        TB.Append(Torrent);
+                        TB.Append("',");
+
+                        TB.Append("'");
+                        TB.Append(Magnet);
+                        TB.Append("',");
+
+
+
+                        TB.Append("'");
+                        TB.Append(Size);
+                        TB.Append("',");
+
+                        TB.Append("'");
+                        TB.Append(Time);
+                        TB.Append("',");
+
+
+                        TB.Append("'");
+                        TB.Append(Up);
+                        TB.Append("',");
+
+                        TB.Append("'");
+                        TB.Append(Leeches);
+                        TB.Append("',");
+
+                        TB.Append("'");
+                        TB.Append(Complete);
+                        TB.Append("')");
+                        /*  Cmd.CommandText = TB.ToString();
+                          Cmd.ExecuteNonQuery();*/
+                        command.CommandText = TB.ToString();
+                        command.ExecuteNonQuery();
+                    }
+                    
                 }
-                else
-                {
-                    Magnet = HtmlNode.CreateNode(temp.SelectSingleNode(@"//td[3]").InnerHtml).SelectSingleNode("//a[2]").Attributes["href"].Value;
-                }
-                var Size = temp.SelectSingleNode(@"//td[4]").InnerHtml;
-                var Time = temp.SelectSingleNode(@"//td[5]").InnerHtml;
-                var Up = temp.SelectSingleNode(@"//td[6]").InnerHtml;
-                var Leeches = temp.SelectSingleNode(@"//td[7]").InnerHtml;
-                var Complete = temp.SelectSingleNode(@"//td[8]").InnerHtml;
-                var TB = new StringBuilder();
-                TB.Append(@"insert into torrent(CLass,title,ImageSrc,Address,Name,Torrent,Magnet,Size,Time,Up,Leeches,Complete) values(");
-                TB.Append("'");
-                TB.Append(Class);
-                TB.Append("',");
-
-                TB.Append("'");
-                TB.Append(title);
-                TB.Append("',");
-
-                TB.Append("'");
-                TB.Append(Imgsrc);
-                TB.Append("',");
-
-                TB.Append("'");
-                TB.Append(Address);
-                TB.Append("',");
-
-                TB.Append("N\'" + name + "\',");
-
-                TB.Append("'");
-                TB.Append(Torrent);
-                TB.Append("',");
-
-                TB.Append("'");
-                TB.Append(Magnet);
-                TB.Append("',");
-
-
-
-                TB.Append("'");
-                TB.Append(Size);
-                TB.Append("',");
-
-                TB.Append("'");
-                TB.Append(Time);
-                TB.Append("',");
-
-
-                TB.Append("'");
-                TB.Append(Up);
-                TB.Append("',");
-
-                TB.Append("'");
-                TB.Append(Leeches);
-                TB.Append("',");
-
-                TB.Append("'");
-                TB.Append(Complete);
-                TB.Append("')");
-                Cmd.CommandText = TB.ToString();
-                Cmd.ExecuteNonQuery();
-
-            }
-            c.Close();
+                trans.Commit();
+                connection.Close();
+                stopWatch.Stop();
+            }).Start();
+            //c.Close();
             url = "https://sukebei.nyaa.si/";
             //go();
         }
@@ -262,6 +301,11 @@ namespace NyaaAnalyse
             {
                 return null;
             }
+        }
+
+        private void 退出_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
